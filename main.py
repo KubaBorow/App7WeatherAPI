ae999cf6a4bfaee6f33f3297033eba8930d6b029
KubaBorow <jakubrcam@gmail.com>
2024-10-31 13:52:53 +0100

    Initial commit after few changes.

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def about(station, date):
 def contact():
     return render_template("contact.html")
 
-
+# Jeśli chcemy uruchomić kilka aplikacji na raz musimy wyszczerólnić port na którym działają, np port = 5001,
+# domyslnie jest ustawiony port 5000
 if __name__ == "__main__":
     app.run(debug=True)
